46d7bf4e46a7dddcfe40f290757a7e0a02d77ed9
Fernando Alberto Lerma Sarabia <fiernando_lerma@hotmail.com>
2022-02-23 20:08:24 -0700

    done

--- test/orm_test.dart
+++ test/orm_test.dart
@@ -1,9 +1,11 @@
 import 'package:sqlite_orm_example_app/database/models.dart';
 
-void main() async {}
+void main() async {
+
+}
 
 Future<int?> _createRestaurant() async {
-  return await Restaurant(name: "Notebooks").save();
+  return await Restaurant(name: "Sopes el autentico").save();
 }
 
 Future<List<Restaurant>?> _getRestaurants() async {
